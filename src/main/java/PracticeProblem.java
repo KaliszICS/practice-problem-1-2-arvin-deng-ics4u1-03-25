4b3ce77f1277faace9c0d8c8f7a65b3937eac37d
arvin-deng-ics4u1-03-25 <arvin.deng@student.tdsb.on.ca>
2025-03-05 03:20:21 +0000

    answer

--- src/main/java/PracticeProblem.java
+++ src/main/java/PracticeProblem.java
@@ -1,9 +1,36 @@
 public class PracticeProblem {
 
-	public static void main(String args[]) {
-
+	public static boolean validIndex(int[] arr, int index) {
+		
+		try {
+			arr[index] = arr[index];
+			return true;
+		}
+		catch (ArrayIndexOutOfBoundsException e) {
+			return false;
+		}
 	}
 
+	public static int divide(int x, int y) {
+
+		try {
+			return x / y;
+		}
+		catch (ArithmeticException e) {
+			return 0;
+		}
+	}
+
+	public static int safeConvertStringtoInt(String s) {
+
+		try {
+			return Integer.parseInt(s);
+		}
+		catch (NumberFormatException e){
+			return 0;
+		}
+	}
+}
 	
 
-}
+
